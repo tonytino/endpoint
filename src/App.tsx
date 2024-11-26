d24e836c3aca1f40c46fe169b02c4de4e278bbee
Anthony Hernandez <tony91139@gmail.com>
2024-11-26 00:41:34 -0700

    feat: add logic for grouping todos based on completion status

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchTodos } from './lib/api';
+import { type Todo } from './types/Todo';
 import classes from './App.module.css';
 
 function App() {
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [pendingTodos, setPendingTodos] = useState<Todo[]>([]);
 
   // Fetch the initial state of the todos
   useEffect(() => {
@@ -13,8 +16,27 @@ function App() {
       const todosJson = await fetchTodos();
 
       if (todosJson && isInitializing) {
-        console.log(todosJson);
+        const groupedTodos = todosJson.reduce<{
+          completed: Todo[];
+          pending: Todo[];
+        }>(
+          (groupedTodos, currentTodo) => {
+            if (currentTodo.isComplete) {
+              groupedTodos.completed.push(currentTodo);
+            } else {
+              groupedTodos.pending.push(currentTodo);
+            }
 
+            return groupedTodos;
+          },
+          {
+            completed: [],
+            pending: [],
+          }
+        );
+
+        setCompletedTodos(groupedTodos.completed);
+        setPendingTodos(groupedTodos.pending);
         setIsLoading(false);
       }
     }
@@ -25,6 +47,11 @@ function App() {
       isInitializing = false;
     };
   }, []);
+
+  console.log({
+    completedTodos,
+    pendingTodos,
+  });
 
   return (
     <div>
