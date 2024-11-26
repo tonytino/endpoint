7f0af0f060115ba562d9e5ccdec1347d61866849
Anthony Hernandez <tony91139@gmail.com>
2024-11-26 00:32:42 -0700

    feat: fetch initial state of todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from 'react';
+import { fetchTodos } from './lib/api';
+
 function App() {
+  // Fetch the initial state of the todos
+  useEffect(() => {
+    let isInitializing = true;
+
+    async function fetchData() {
+      const todosJson = await fetchTodos();
+
+      if (todosJson && isInitializing) {
+        console.log(todosJson);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      isInitializing = false;
+    };
+  }, []);
+
   return <></>;
 }
 
