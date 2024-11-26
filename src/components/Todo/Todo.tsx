c3985e8858c30346915d0382fec4433d99315a6a
Anthony Hernandez <tony91139@gmail.com>
2024-11-26 01:11:26 -0700

    feat: add styling to indicate status of todos

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { type Todo } from '../../types/Todo';
 import { CheckedBox } from '../CheckedBox';
 import { UncheckedBox } from '../UncheckedBox';
@@ -11,13 +12,26 @@ export function Todo(props: TodoProps) {
   const { todo } = props;
   const { description, dueDate, isComplete } = todo;
 
+  const currentTime = new Date();
+  /**
+   * Passing null to new Date returns a Date object representing the Unix epoch. Instead, we'll default to Invalid Date for such a scenario and check if dueDate is truthy before attempting to use this variable
+   */
+  const parsedDueDate = new Date(dueDate || NaN);
+  const isOverdue = dueDate && parsedDueDate.valueOf() < currentTime.valueOf();
+
   return (
-    <li className={classes.Todo}>
+    <li
+      className={clsx({
+        [classes.Todo]: true,
+        [classes.completed]: isComplete,
+        [classes.overdue]: isOverdue,
+      })}
+    >
       <button className={classes.TodoToggle}>
         {isComplete ? <CheckedBox /> : <UncheckedBox />}
       </button>
 
-      {description}
+      <span className={classes.description}>{description}</span>
 
       {dueDate}
     </li>
